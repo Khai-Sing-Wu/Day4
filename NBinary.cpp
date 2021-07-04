6cce2e6990f810d6d2df90595f8220995457e4a0
Khai-Sing-Wu <86360549+Khai-Sing-Wu@users.noreply.github.com>
2021-07-04 19:32:52 +0530

    Add files via upload

---
+++ NBinary.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void generateBinaryBitsHelper(vector<string> &result, string &bitsSoFar, int index, int N)
+{
+    if (index == N)
+    {
+        result.push_back(bitsSoFar);
+        return;
+    }
+    bitsSoFar[index] = '0';
+    generateBinaryBitsHelper(result, bitsSoFar, index + 1, N);
+
+    bitsSoFar[index] = '1';
+    generateBinaryBitsHelper(result, bitsSoFar, index + 1, N);
+    bitsSoFar[index] = '.';
+}
+
+vector<string> generateBinaryBits(int N)
+{
+    vector<string> result;
+    string bitsSoFar(N, '.');
+    generateBinaryBitsHelper(result, bitsSoFar, 0, N);
+    return result;
+}
+
+int main()
+{
+    int number;
+    cin >> number;
+    vector<string> allBinaryBits = generateBinaryBits(number);
+    for (string possible : allBinaryBits)
+    {
+        cout << possible << " ";
+    }
+        
+}
